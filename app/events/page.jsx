3731cff64691118004cf8628194bfe35a1c1b4a1
ollhamm <ilhamkusmayadi65@gmail.com>
2024-07-30 23:50:57 +0700

    update events

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,9 +1,84 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+const Card = ({ imgSrc, onClick }) => (
+  <div className="relative w-48 h-48 m-2 cursor-pointer" onClick={onClick}>
+    <Image
+      src={imgSrc}
+      alt="Poster"
+      layout="fill"
+      objectFit="cover"
+      className="rounded-lg"
+    />
+  </div>
+);
+
+const Modal = ({ imgSrc, onClose }) => (
+  <div
+    className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+    onClick={onClose}
+  >
+    <div className="relative w-full h-full md:w-auto md:h-auto">
+      <Image src={imgSrc} alt="Poster" layout="fill" objectFit="contain" />
+    </div>
+  </div>
+);
 
 const Events = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleCardClick = (imgSrc) => {
+    setSelectedImage(imgSrc);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <div className=" bg-slate-100 flex flex-col items-center justify-center min-h-screen">
-      event coming soon!
+    <div className="flex flex-col bg-slate-100 mt-20">
+      <div className="flex flex-col items-center justify-center">
+        <div>
+          <Image
+            src="/images/lomba2.png"
+            height={600}
+            width={600}
+            alt="foto bareng"
+            className="w-200 h-200 md:h-600 px-6 md:px-0 md:w-600"
+          />
+        </div>
+        <div className="flex flex-row px-8 md:px-48 text-xs md:text-sm rounded-xl shadow-lg p-6 items-center justify-center">
+          <Image
+            src="/images/logo-hut-ri.png"
+            alt="logo 79"
+            height={100}
+            width={200}
+          />
+        </div>
+      </div>
+      <div className="flex flex-2 flex-col items-center justify-center h-screen">
+        <div className="font-bold text-lg md:text-2xl mb-10 text-neutral-400 underline">
+          Ayo Ikuti dan Meriahkan!
+        </div>
+        <div className="flex flex-row items-center justify-center gap-4 flex-wrap">
+          <Card
+            imgSrc="/images/lomba2.png"
+            onClick={() => handleCardClick("/images/lomba2.png")}
+          />
+          <Card
+            imgSrc="/images/lomba2.png"
+            onClick={() => handleCardClick("/images/lomba2.png")}
+          />
+          <Card
+            imgSrc="/images/lomba2.png"
+            onClick={() => handleCardClick("/images/lomba2.png")}
+          />
+        </div>
+      </div>
+      {selectedImage && (
+        <Modal imgSrc={selectedImage} onClose={handleCloseModal} />
+      )}
     </div>
   );
 };
